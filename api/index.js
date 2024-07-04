cabb080ede3de11897a75deb50fcb5efe7c022d1
Nest5 <nestoramerican1@gmail.com>
2024-07-04 17:48:44 +0200

    cambio en el get

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/api', (req, res)=>{
-    res.send("Hola caracola");
+    res.send("Hola caracola ¿quieres uns pepsi cola?");
 });
 
 app.listen(port, ()=>{
